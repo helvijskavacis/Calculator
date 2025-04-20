93c82485c0dc5e37d5ecd61d5f86ec34611e78b8
Helvijs Kavacis <helvijs.kavacis@gmail.com>
2025-04-20 11:36:31 +0300

    Finished Calculator with History

--- 2nd_calc.py
+++ 2nd_calc.py
@@ -1,29 +1,44 @@
+# Standard lib
 import math
-#Math functions
-def add(x,y): return x+y
-def subtract(x,y): return x-y
-def multiply(x,y): return x*y
-def divide(x,y): 
+
+
+# Math functions
+def add(x, y): return x + y
+
+
+def subtract(x, y): return x - y
+
+
+def multiply(x, y): return x * y
+
+
+def divide(x, y):
     try:
-         return x/y
+        return x / y
     except ZeroDivisionError:
-         print("Can't divide by zero!")
-         return None
-def exponentiation(x,y):
+        print("Can't divide by zero!")
+        return None
+
+
+def exponentiation(x, y):
     try:
-         return x**y
+        return x ** y
     except ZeroDivisionError:
-         print("Can't perform exponention of 0 by a negative power.")
-         return None
+        print("Can't perform exponention of 0 by a negative power.")
+        return None
+
+
 def square_root(x):
     if x < 0:
         return f"Can't calculate sqrt({x}), as it must be positive!"
     return math.sqrt(x)
 
-#see history
+
+# See history
 def history():
-    while True: 
-        input_history = input("Would you like to see the calculation history? y/n")
+    while True:
+        input_history = input("Would you like to see the "
+                              "calculation history? y/n")
         if input_history == "y":
             with open("Calculations.txt", "r") as file:
                 for line in file:
@@ -32,10 +47,11 @@ def history():
         elif input_history == "n":
             print("Thank you for using the calculator!")
             break
-        else: 
+        else:
             print("Oops, try again: ")
 
-#User inserts variable
+
+# User inserts variable
 def user_variable():
     while True:
         try:
@@ -44,16 +60,19 @@ def user_variable():
         except ValueError:
             print("Please, re-enter an integer: ")
 
-#User inserts operator
+
+# User inserts operator
 def user_operator():
     while True:
         operator = input("Choose operator: ( +; -; *; /; **; sqrt; = )")
-        if operator in ["+", "-", "*", "/", "**", "sqrt", "="]: #check if operator is correct format
+        # Check if operator is correct format
+        if operator in ["+", "-", "*", "/", "**", "sqrt", "="]:
             return operator
         else:
             print("Please re-enter a valid operator: ")
 
-#Writing user input in the list
+
+# Writing user input in the list
 def write_in_list():
     calc_list = []
     while True:
@@ -65,30 +84,41 @@ def write_in_list():
     return calc_list
 
 
+# Iterates through the list and calls for calculations where appropriate.
 def calculate_list(calc_list):
-    counter = -1
     total = calc_list[0]
-    for var in calc_list[2::2]:
-        counter += 2
-        if calc_list[counter] == "+":
-            total = add(total, var)
-        elif calc_list[counter] == "-":
-            total = subtract(total, var)
-        elif calc_list[counter] == "*":
-            total = multiply(total, var)
-        elif calc_list[counter] == "/":
-            total = divide(total, var)
-        elif calc_list[counter] == "**":
-            total = total ** var
-        elif calc_list[counter] == "sqrt":
+    i = 1
+    while i < len(calc_list):
+        operator = calc_list[i]
+        if operator == "+":
+            total = add(total, calc_list[i + 1])
+            i += 2
+        elif operator == "-":
+            total = subtract(total, calc_list[i + 1])
+            i += 2
+        elif operator == "*":
+            total = multiply(total, calc_list[i + 1])
+            i += 2
+        elif operator == "/":
+            total = divide(total, calc_list[i + 1])
+            i += 2
+        elif operator == "**":
+            total = exponentiation(total, calc_list[i + 1])
+            i += 2
+        elif operator == "sqrt":
             total = square_root(total)
+            i += 1
             break
-        else: # when it's (=)
+        elif operator == "=":
+            break
+        else:
+            print(f"Unknown operator {operator}")
             break
     calc_list.append(total)
     with open("Calculations.txt", "a") as file:
         file.write(f"\n{" ".join(map(str, calc_list))}")
     print(' '.join(map(str, calc_list)))
+
 
 calculate_list(write_in_list())
 history()
